a658ee886e93175eb5e32f24709a29b8c64304d7
Henrik Jachobsen <hjachobsen@gmail.com>
2017-05-03 09:28:24 +0200

    Correct string conversion when determining window name

--- src/window.cc
+++ src/window.cc
@@ -1,9 +1,25 @@
 #include "window.h"
 #include "os.h"
 #include "process.h"
+#include <string>
 
 #if defined(IS_MACOSX)
     #include <QuartzCore/QuartzCore.h>
+
+    bool ToUtf8(const CFStringRef str16, std::string* str8) {
+        if ((nullptr == str16) || (nullptr == str8)) {
+            return false;
+        }
+        size_t maxlen = CFStringGetMaximumSizeForEncoding(CFStringGetLength(str16),
+                                                        kCFStringEncodingUTF8) + 1;
+        std::unique_ptr<char[]> buffer(new char[maxlen]);
+        if (!buffer || !CFStringGetCString(str16, buffer.get(), maxlen,
+                                         kCFStringEncodingUTF8)) {
+            return false;
+        }
+        str8->assign(buffer.get());
+        return true;
+    }
 #endif
 
 std::vector<MMWindow> getWindowList() {
@@ -25,11 +41,16 @@ std::vector<MMWindow> getWindowList() {
         MMWindow window;
         CFNumberGetValue(windowOwnerPIDRef, kCFNumberIntType, &window.pid);
         CFNumberGetValue(windowNumberRef, kCFNumberIntType, &window.number);
-        window.name = CFStringGetCStringPtr(windowNameRef, kCFStringEncodingUTF8);
         CGRect windowBounds;
         CGRectMakeWithDictionaryRepresentation(windowBoundsRef, &windowBounds);
         window.bounds = MMRectMake(windowBounds.origin.x, windowBounds.origin.y, windowBounds.size.width, windowBounds.size.height);
         window.ownerIsFocused = (int)window.pid == focusedPID;
+
+        std::string windowName;
+        if (!ToUtf8(windowNameRef, &(windowName))) {
+            continue;
+        }
+        window.name = strcpy((char*)malloc(windowName.length()+1), windowName.c_str());;
 
         retVal.push_back(window);
     }
